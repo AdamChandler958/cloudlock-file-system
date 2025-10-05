0f990653a5bb978b6ffb4dc87c1f029473e72886
Adam Chandler <adam.c02pro@gmail.com>
2025-10-06 09:51:27 +1030

    feat: adding logic to delete entire buckets for cleanup

--- app/src/endpoints/buckets.py
+++ app/src/endpoints/buckets.py
@@ -4,6 +4,8 @@ from typing import Annotated
 from minio import Minio
 from app.src.models.BucketInfo import BucketInfo
 
+from app.src.endpoints.files import get_files_in_bucket, recurssive_delete_objects
+
 router = APIRouter(prefix="/buckets", tags=["buckets", "storage"])
 
 
@@ -26,3 +28,18 @@ def create_bucket(
         raise HTTPException(422, f"Making bucket failed with exception: {e}")
 
     return {"message": f"Bucket {bucket_info.bucket_name} created successfully"}
+
+
+@router.delete("/delete_bucket")
+def delete_bucket(bucket_name: str, client: Annotated[Minio, Depends(get_client)]):
+    if client.bucket_exists(bucket_name=bucket_name):
+        if get_files_in_bucket(bucket_name, client) is not None:
+            recurssive_delete_objects(bucket_name, client)
+        try:
+            client.remove_bucket(bucket_name=bucket_name)
+        except Exception as e:
+            raise HTTPException(422, f"An error occured while removing bucket: {e}")
+    else:
+        raise HTTPException(404, "Bucket does not exist.")
+
+    return {"message": f"Bucket {bucket_name} deleted successfully"}
--- app/src/endpoints/files.py
+++ app/src/endpoints/files.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, HTTPException
 from app.src.minio_client import get_client
 from typing import Annotated
 import os
@@ -34,3 +34,41 @@ def single_file_upload(
         }
     except Exception as e:
         return {"error": str(e)}
+
+
+@router.delete("/delete/{bucket_name}")
+def single_file_delete(
+    bucket_name: str, object_name: str, client: Annotated[Minio, Depends(get_client)]
+):
+    if client.get_object(bucket_name, object_name) is not None:
+        try:
+            client.remove_object(bucket_name, object_name)
+        except Exception as e:
+            raise HTTPException(422, f"An error occured while removing object: {e}")
+    else:
+        raise HTTPException(404, f"Object {object_name} does not exist")
+
+    return {
+        "message": f"Object {object_name} deleted from bucket {bucket_name} successfully"
+    }
+
+
+@router.get("/files")
+def get_files_in_bucket(
+    bucket_name: str, client: Annotated[Minio, Depends(get_client)]
+):
+    bucket_list = client.list_objects(bucket_name=bucket_name, recursive=True)
+    return bucket_list
+
+
+@router.delete("/delete_all/{bucket_name}")
+def recurssive_delete_objects(
+    bucket_name: str, client: Annotated[Minio, Depends(get_client)]
+):
+    bucket_list = get_files_in_bucket(bucket_name, client)
+    for obj in bucket_list:
+        single_file_delete(
+            bucket_name=bucket_name, object_name=obj.object_name, client=client
+        )
+
+    return {"message": f"All items in {bucket_name} deleted successfully."}
